d470eb6ded7e437639450f7bf11497e4adb3c82e
Abdullahinur <aabdullahinur@hotmail.com>
2018-04-02 18:04:47 +0300

    fix functionality bugs, not saving user now fixed

--- run.py
+++ run.py
@@ -2,17 +2,17 @@
 from user import User
 
 
-def create_user(first_name, last_name, email):
+def create_user(fname, lname, phone, email):
     '''
-    Function to create a new user
+    Function to create a new contact
     '''
-    new_user = User(first_name, last_name, email)
+    new_user = User(fname, lname, phone, email)
     return new_user
 
 
 def save_users(user):
     '''
-    Function to save user
+    Function to save contact
     '''
     user.save_user()
 
@@ -24,11 +24,11 @@ def del_user(user):
     user.delete_user()
 
 
-def find_user(fname):
+def find_user(number):
     '''
     Function that finds a user by their first name
     '''
-    return User.find_by_firstName(fname)
+    return User.find_by_number(number)
 
 
 def check_existing_users(fname):
@@ -59,94 +59,131 @@ def main():
     print("This will store your credentails and generate a password for you")
     print("My name is Ab,")
     print("What's yours")
+
     user_name = input()
 
-    print(f"Hello {user_name}.")
-    print("You can use this program by typing some simple phrases")
-    print("Here are some phrases to get you started: # new - create new user, # display - displays the users, # find - find a user, # del - del a user, # copy - copy user info, # exit - exit the password locker")
+    print(f"Hello {user_name}. What would you like to do?")
 
-    phrases = input().lower()
+    print("\n")
 
-    if phrases == 'new':
-        print("New User")
-        print("-" * 10)
+    while True:
+        print(f"Hello {user_name}.")
+        print("You can use this program by typing some simple phrases")
+        print("Here are some phrases to get you started: # new - create new user, # display - displays the users, # find - find a user, # del - del a user, # copy - copy user info, # exit - exit the password locker")
 
-        print("What is your First Name:")
-        first_name = input()
+        phrases = input().lower()
 
-        print("What is your last Name:")
-        last_name = input()
+        if phrases == 'new':
+            print("New User")
+            print("-" * 10)
 
-        print("What is your email address:")
-        email = input()
+            print("First name .....")
+            first_name = input()
 
-        # Create and save a new user
-        save_users(create_user(first_name, last_name, email))
+            print("Last name .....")
+            last_name = input()
 
-        print("\n")
-        print("\n")
-        print(f"Welcome NEW USER: {first_name}")
-        print("\n")
+            print("Phone number .....")
+            phone_number = input()
 
-    elif phrases == 'display':
+            print("Email address .......")
+            email = input()
 
-        if display_users():
-
-            print("Here is a list of current users")
-            print("\n")
-            for user in display_users():
-
-                print(f"{user.first_name} {user.last_name}.............{user.email}")
+            # Create and save new contact
+            save_users(create_user(first_name, last_name, phone_number, email))
 
             print("\n")
+
+            print(f"Welcome NEW USER: {first_name} {last_name}")
+
+            print("\n")
+
+        elif phrases == 'display':
+
+            if display_users():
+
+                print("Here is a list of current users")
+
+                print("\n")
+
+                for user in display_users():
+
+                    print(f"{user.first_name} {user.last_name} .............{user.phone_number}")
+
+                print("\n")
+
+            else:
+                print("\n")
+
+                print("There aren't any users yet")
+
+                print("\n")
+
+        elif phrases == 'find':
+
+            print("Enter the number you want to search for")
+
+            search_number = input()
+
+            if check_existing_users(search_number):
+
+                search_user = find_user(search_number)
+
+                print(f"{search_user.first_name} {search_user.last_name}")
+
+                print('-' * 20)
+
+                print(f"Phone number .....{search_user.phone_number}")
+
+                print(f"Email ......{search_user.email}")
+
+            else:
+                print("That User does not exist")
+
+        elif phrases == 'del':
+
+            print("Enter the number you want to delete")
+
+            delete_number = input()
+
+            if check_existing_users(delete_number):
+
+                search_delete_user = find_user(delete_number)
+
+                print(f"The following user: {search_delete_user.first_name} {search_delete_user.last_name} has been deleted")
+
+                delete_user(search_delete_user)
+
+            else:
+                print("That contact does not exist")
+
+        elif phrases == 'copy':
+
+            print("Enter the number for the email you want to copy")
+
+            search_number_for_email = input()
+
+            if check_existing_users(search_number_for_email):
+
+                user_found = find_user(search_number_for_email)
+
+                user_email = copy_existing_email(search_number_for_email)
+
+                paste_user_email = pyperclip.paste()
+
+                print(f"{ pyperclip.paste()} belongs to {user_found.first_name} {user_found.last_name}")
+
+            else:
+
+                print("The contact does not exist")
+
+        elif short_code == 'exit':
+
+            print("Bye .........")
 
         else:
 
-            print("\n")
-
-            print("There aren't any users yet")
-
-            print("\n")
-
-    elif phrases == 'find':
-
-        print("Enter the first name of the user you want to search for")
-
-        search_firstName = input()
-
-        if check_existing_users(search_firstName):
-
-            search_user = find_user(search_firstName)
-
-            print(f"{search_user.first_name} {search_user.last_name}")
-
-            print('-' * 20)
-
-            print(f"Email ....{search_user.email}")
-
-        else:
-            print("That user does not exist")
-
-    elif phrases == 'del':
-
-        print("Enter the first name of the user you want to delete:")
-
-        delete_user = input()
-
-        if check_existing_users(delete_user):
-
-            search_delete_user = find_user(delete_user)
-
-            print(f"The following user: {search_delete_user.first_name} {search_delete_user.last_name} has been deleted")
-
-            delete_user(search_delete_user)
-
-        else:
-            print("That user does not exist")
-
-    elif phrases == 'copy':
-
-        print("Enter the numver")
+            print("I really didn't get that. Please use the short codes")
 
 
 if __name__ == '__main__':
--- user.py
+++ user.py
@@ -1,41 +1,74 @@
+import pyperclip
+
+
 class User:
     """
-    class that generates new instances of users.
+    Class that generates new instances of users.
     """
 
-    user_list = []
+    user_list = []  # Empty user list
 
-    def __init__(self, first_name, last_name, email):
+    def __init__(self, first_name, last_name, phone_number, email):
+
+      # docstring removed for simplicity
 
         self.first_name = first_name
         self.last_name = last_name
+        self.phone_number = phone_number
         self.email = email
 
     def save_user(self):
+        '''
+        save_contact method saves contact objects into contact_list
+        '''
 
         User.user_list.append(self)
 
     def delete_user(self):
+        '''
+        delete_contact method deletes a saved contact from the contact_list
+        '''
 
         User.user_list.remove(self)
 
     @classmethod
-    def find_by_firstName(cls, first_name):
+    def find_by_number(cls, number):
+        '''
+        Method that takes in a number and returns a contact that matches that number.
+
+        Args:
+            number: Phone number to search for
+        Returns :
+            Contact of person that matches the number.
+        '''
 
         for user in cls.user_list:
-            if user.first_name == first_name:
+            if user.phone_number == number:
                 return user
 
     @classmethod
-    def user_exist(cls, first_name):
-
+    def user_exist(cls, number):
+        '''
+        Method that checks if a contact exists from the contact list.
+        Args:
+            number: Phone number to search if it exists
+        Returns :
+            Boolean: True or false depending if the contact exists
+        '''
         for user in cls.user_list:
-            if user.first_name == first_name:
+            if user.phone_number == number:
                 return True
 
     @classmethod
     def display_users(cls):
-
+        '''
+        method that returns the user list
+        '''
         return cls.user_list
+
+    @classmethod
+    def copy_email(cls, number):
+        user_found = User.find_by_number(number)
+        pyperclip.copy(user_found.number)
 
         return False
