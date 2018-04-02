c52d45f2bd4aed413e141202305f37f48bbf63bd
Abdullahinur <aabdullahinur@hotmail.com>
2018-04-02 12:49:29 +0300

    create new user instance define properties for user class

---
+++ user.py
@@ -0,0 +1,12 @@
+class User:
+    """
+    class that generates new instances of users.
+    """
+
+    user_list = []
+
+    def __init__(self, first_name, last_name, email):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
